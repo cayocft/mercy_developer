68f422a2945c9ab5590e9d4fa7300db11b38b190
Edgardo Cayo <edgardo.cayo.miranda@cftmail.cl>
2024-05-13 23:08:44 -0400

    Proyecto con tres CRUD Clientes, Usuarios y Servicios

---
+++ Models/Servicio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mercy_developer.Models;
+
+public partial class Servicio
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public int Precio { get; set; }
+
+    public string? Sku { get; set; }
+
+    /// <summary>
+    /// 0:Inactivo ; 1: Activo
+    /// </summary>
+    public int Estado { get; set; }
+
+    public int UsuarioId { get; set; }
+
+    public virtual Usuario Usuario { get; set; } = null!;
+}
